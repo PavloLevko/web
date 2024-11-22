0c186ade40f5d95f5706c2f764bca8e5572dc24b
”Pavlo <levkopavlo@gmail.com>
2024-11-22 11:35:18 +0200

    add new html pages

--- register/urls.py
+++ register/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth import views as auth
 urlpatterns = [
     path('reg/', views.register, name='reg'),
     path('user/',auth.LoginView.as_view(template_name='register/user.html'), name='user'),
-    path('exit/',auth.LogoutView.as_view(template_name='register/exit.html'), name='exit')
+    path('exit/',views.user_exit, name='exit')
 ]
